299b1e7daeb6806b7e41f7cf41f48a43ff3c94ca
yangkangkai <452306233@qq.com>
2018-11-23 23:33:00 +0800

    可以接受文本消息

--- get_access_token.js
+++ get_access_token.js
@@ -1,10 +1,42 @@
-var weixin_url_home = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxc16ceb98ec30575b&secret=6eaaf1a7eb26bf1af7d8033a340ff98e"
+var access_token = "";
+var weixin_url_home = 
+"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxc16ceb98ec30575b&secret=6eaaf1a7eb26bf1af7d8033a340ff98e";
 
+var requestData =   {
+    "button":[
+    {    
+         "type":"click",
+         "name":"今日歌曲",
+         "key":"V1001_TODAY_MUSIC"
+     },
+     {
+          "name":"菜单",
+          "sub_button":[
+          {    
+              "type":"view",
+              "name":"搜索",
+              "url":"http://www.soso.com/"
+           },
+           {
+              "type":"click",
+              "name":"赞一下我们",
+              "key":"V1001_GOOD"
+           }]
+      }]
+}
 
 var request = require('request');
+
 request.get(weixin_url_home,function(err,response,body){
     console.log(typeof body);
     var body_json = JSON.parse(body); //将json类型的字符串转为js对象，字典。
     var access_token = body_json["access_token"];
     console.log(access_token);
+    request.post({
+        url: "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=" + access_token,
+        body: requestData,
+        json: true
+        }, (err, res, body) => {
+        console.log(body)
+    })
 })
---
+++ reply.js
@@ -0,0 +1,30 @@
+const express = require('express'); //express 框架 
+var bodyParser = require('body-parser');
+var xml2js=require('xml2js');
+
+var app = express();//实例express框架
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.post('/', function (req, res) {
+    req.rawBody = '';//添加接收变量
+    var json = {};
+    var xmlParser = new xml2js.Parser({explicitArray : false, ignoreAttrs : true})
+    req.setEncoding('utf8');
+    req.on('data', function (chunk) {
+        req.rawBody += chunk;
+    });
+    req.on('end', function () {
+        // json = xml2js.toJson(req.rawBody);
+        // res.send(JSON.stringify(json));
+        xmlParser.parseString(req.rawBody, function (err, result) {
+            //将返回的结果再次格式化
+             console.log(JSON.stringify(result));
+             res.send("success");
+          });
+    });
+});
+
+//监听80端口
+app.listen(80);
